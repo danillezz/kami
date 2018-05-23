1a202bc7b3ee0030b67e5c35d1cf43bbc1a640dd
TyukinDN <TyukinDN@sovcombank.ru>
2018-05-23 16:01:55 +0300

    Доделаны + отлажены задачи и ввод данных

--- src/main/java/com/example/demo/MainForm.java
+++ src/main/java/com/example/demo/MainForm.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.demo.common.AhovRdTask;
+import com.example.demo.common.BlastTask;
 import com.example.demo.webservice.KamiClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -54,6 +55,8 @@ public class MainForm {
     private JTextField txtWindDirection;
     private JTextField txtWindSpeed;
     private JCheckBox cbxSnowCover;
+    private JCheckBox cbxVehicle;
+    private JCheckBox cbxWeather;
     private final KamiClient kamiClient;
 
 
@@ -69,36 +72,81 @@ public class MainForm {
             public void actionPerformed(ActionEvent e) {
                 try {
                     AhovRdTask ahovRdTask = (AhovRdTask) kamiClient.getAhov(cbBund.getSelectedItem().toString(),
-                            Double.valueOf(txtHeight.getText()),
-                            Double.valueOf(txtArea.getText()),
+                            txtHeight.getText(),
+                            txtArea.getText(),
                             txtChemicalAgentName.getText(),
-                            Double.valueOf(txtChemLatitude.getText()),
-                            Double.valueOf(txtChemLongitude.getText()),
-                            Double.valueOf(txtQuantity.getText()),
+                            txtChemLatitude.getText(),
+                            txtChemLongitude.getText(),
+                            txtQuantity.getText(),
                             txtQuantityUnit.getText(),
                             cbxIsPopulationInformed.isSelected(),
                             txtPredictionDateTime.getText(),
-                            Long.valueOf(txtStartNeutralizationTime.getText()),
-                            Long.valueOf(txtTimeToNeutralization.getText()),
-                            Double.valueOf(txtFillingTime.getText()),
-                            Double.valueOf(txtTravelTime.getText()),
-                            Double.valueOf(txtVolume.getText()),
-                            Double.valueOf(txtWorkTime.getText())).getExecuteResult();
+                            txtStartNeutralizationTime.getText(),
+                            txtTimeToNeutralization.getText(),
+                            cbxVehicle.isSelected(),
+                            txtFillingTime.getText(),
+                            txtTravelTime.getText(),
+                            txtVolume.getText(),
+                            txtWorkTime.getText(),
+                            txtId.getText(),
+                            txtTitle.getText(),
+                            txtDateTime.getText(),
+                            txtAdvancedInfo.getText(),
+                            cbxWeather.isSelected(),
+                            txtAirPressure.getText(),
+                            txtAirTemperature.getText(),
+                            cbAtmosphereState.getSelectedItem().toString(),
+                            txtCloudiness.getText(),
+                            txtWeatherDateTime.getText(),
+                            txtPrecipitation.getText(),
+                            txtWindSpeed.getText(),
+                            txtWindDirection.getText(),
+                            cbxSnowCover.isSelected()).getExecuteResult();
                     txtAhovResult.setText(ahovRdTask.getResults().toString());
-                } catch (DatatypeConfigurationException e1) {
+                } catch (Exception e1) {
                     e1.printStackTrace();
-                } catch (ParseException e1) {
+                    JOptionPane.showMessageDialog(null, e1.getMessage());
+                }
+            }
+        });
+        btnBlast.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    BlastTask blastTask = (BlastTask) kamiClient.getBlast(txtBeginningRescueOperation.getText(),
+                            txtDurationRescueOperation.getText(),
+                            txtExplosiveAgent.getText(),
+                            txtBlastLatitude.getText(),
+                            txtBlastLongitude.getText(),
+                            txtMassOfAgent.getText(),
+                            txtId.getText(),
+                            txtTitle.getText(),
+                            txtDateTime.getText(),
+                            txtAdvancedInfo.getText(),
+                            cbxWeather.isSelected(),
+                            txtAirPressure.getText(),
+                            txtAirTemperature.getText(),
+                            cbAtmosphereState.getSelectedItem().toString(),
+                            txtCloudiness.getText(),
+                            txtWeatherDateTime.getText(),
+                            txtPrecipitation.getText(),
+                            txtWindSpeed.getText(),
+                            txtWindDirection.getText(),
+                            cbxSnowCover.isSelected()).getExecuteResult();
+                    txtAhovResult.setText(blastTask.getResult().toString());
+                } catch (Exception e1) {
                     e1.printStackTrace();
+                    JOptionPane.showMessageDialog(null, e1.getMessage());
                 }
             }
         });
         cbBund.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (cbBund.getSelectedIndex()==0) {
+                if (cbBund.getSelectedIndex() == 0) {
                     txtArea.setEnabled(false);
                     txtHeight.setEnabled(false);
-                } else if (cbBund.getSelectedIndex()==1) {
+                } else if (cbBund.getSelectedIndex() == 1) {
                     txtArea.setEnabled(false);
                     txtHeight.setEnabled(true);
                 } else {
@@ -114,6 +162,63 @@ public class MainForm {
                     cbxIsPopulationInformed.setText("Да");
                 else
                     cbxIsPopulationInformed.setText("Нет");
+            }
+        });
+
+        cbxSnowCover.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (cbxSnowCover.isSelected())
+                    cbxSnowCover.setText("Есть");
+                else
+                    cbxSnowCover.setText("Нет");
+            }
+        });
+
+        cbxVehicle.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (cbxVehicle.isSelected()) {
+                    cbxVehicle.setText("Учитывать");
+                    txtFillingTime.setEnabled(true);
+                    txtTravelTime.setEnabled(true);
+                    txtVolume.setEnabled(true);
+                    txtWorkTime.setEnabled(true);
+                } else {
+                    cbxVehicle.setText("Не учитывать");
+                    txtFillingTime.setEnabled(false);
+                    txtTravelTime.setEnabled(false);
+                    txtVolume.setEnabled(false);
+                    txtWorkTime.setEnabled(false);
+                }
+            }
+        });
+        cbxWeather.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (cbxWeather.isSelected()) {
+                    cbxWeather.setText("Учитывать");
+                    txtAirPressure.setEnabled(true);
+                    txtAirTemperature.setEnabled(true);
+                    cbAtmosphereState.setEnabled(true);
+                    txtCloudiness.setEnabled(true);
+                    txtWeatherDateTime.setEnabled(true);
+                    txtPrecipitation.setEnabled(true);
+                    txtWindSpeed.setEnabled(true);
+                    txtWindDirection.setEnabled(true);
+                    cbxSnowCover.setEnabled(true);
+                } else {
+                    cbxWeather.setText("Не учитывать");
+                    txtAirPressure.setEnabled(false);
+                    txtAirTemperature.setEnabled(false);
+                    cbAtmosphereState.setEnabled(false);
+                    txtCloudiness.setEnabled(false);
+                    txtWeatherDateTime.setEnabled(false);
+                    txtPrecipitation.setEnabled(false);
+                    txtWindSpeed.setEnabled(false);
+                    txtWindDirection.setEnabled(false);
+                    cbxSnowCover.setEnabled(false);
+                }
             }
         });
     }
--- src/main/java/com/example/demo/webservice/KamiClient.java
+++ src/main/java/com/example/demo/webservice/KamiClient.java
@@ -1,45 +1,122 @@
 package com.example.demo.webservice;
 
 import com.example.demo.common.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 import org.springframework.ws.soap.client.core.SoapActionCallback;
 
-import javax.xml.bind.JAXBElement;
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
-import javax.xml.namespace.QName;
-import java.math.BigDecimal;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class KamiClient extends WebServiceGatewaySupport {
 
-    public ExecuteResponse getAhov(String bund, double height, double area, String chemicalAgentName, double latitude, double longitude, double quantity, String quantityUnit, boolean isPopulationInformed, String predictionDateTime, long startNeutralizationTime, long timeToNeutralization, double fillingTime, double travelTime, double volume, double workTime) throws DatatypeConfigurationException, ParseException {
-
+    private XMLGregorianCalendar StrToXMLGregorianCalendar(String dateTime) throws ParseException, DatatypeConfigurationException {
         DateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        Date date = format.parse(predictionDateTime);
+        Date date = format.parse(dateTime);
         GregorianCalendar cal = new GregorianCalendar();
         cal.setTime(date);
-        XMLGregorianCalendar xmlGregCal = DatatypeFactory.newInstance().newXMLGregorianCalendar(cal);
+        return DatatypeFactory.newInstance().newXMLGregorianCalendar(cal);
+    }
 
+    private Task setCommonInfo(Task request, String id, String title, String dateTime, String advancedInfo, boolean isWeatherEnabled, String airPressure, String airTemperature, String atmosphereState, String cloudiness, String weatherDateTime, String precipitation, String windSpeed, String windDirection, boolean snowCover) {
+        if (!id.isEmpty())
+            request.setId(id);
+        if (!title.isEmpty())
+            request.setTitle(title);
+        if (!dateTime.isEmpty()) {
+            try {
+                request.setDateTime(StrToXMLGregorianCalendar(dateTime));
+            } catch (Exception ignored) {
+
+            }
+        }
+        if (!advancedInfo.isEmpty())
+            request.setAdvancedInfo(advancedInfo);
+
+        if (isWeatherEnabled) {
+            Weather weather = new Weather();
+            try {
+                weather.setAirPressure(Double.valueOf(airPressure));
+            } catch (Exception ignored) {
+
+            }
+            try {
+                weather.setAirTemperature(Double.valueOf(airTemperature));
+            } catch (Exception ignored) {
+
+            }
+            switch (atmosphereState) {
+                case "Изотермия":
+                    weather.setAtmosphereState(AtmosphereState.ISOTHERMAL);
+                    break;
+                case "Инверсия":
+                    weather.setAtmosphereState(AtmosphereState.INVERSION);
+                    break;
+                case "Конвекция":
+                    weather.setAtmosphereState(AtmosphereState.CONVECTION);
+                    break;
+                default:
+                    break;
+            }
+
+            try {
+                weather.setCloudiness(Integer.valueOf(cloudiness));
+            } catch (Exception ignored) {
+
+            }
+            try {
+                weather.setPrecipitation(Double.valueOf(precipitation));
+            } catch (Exception ignored) {
+
+            }
+            try {
+                weather.setWindSpeed(Double.valueOf(windSpeed));
+            } catch (Exception ignored) {
+
+            }
+            try {
+                weather.setWindDirection(Double.valueOf(windDirection));
+            } catch (Exception ignored) {
+
+            }
+            try {
+                weather.setDateTime(StrToXMLGregorianCalendar(weatherDateTime));
+            } catch (Exception ignored) {
+
+            }
+            weather.setSnowCover(snowCover);
+            request.setWeather(weather);
+        }
+
+        return request;
+    }
+
+    //-------------расчет задачи АХОВ-----------------------------------------------------------------------------------
+    public ExecuteResponse getAhov(String bund, String height, String area, String chemicalAgentName, String latitude, String longitude, String quantity, String quantityUnit, boolean isPopulationInformed, String predictionDateTime, String startNeutralizationTime, String timeToNeutralization, boolean isVehicleEnabled, String fillingTime, String travelTime, String volume, String workTime, String id, String title, String dateTime, String advancedInfo, boolean isWeatherEnabled, String airPressure, String airTemperature, String atmosphereState, String cloudiness, String weatherDateTime, String precipitation, String windSpeed, String windDirection, boolean snowCover) throws Exception {
         ChemicalStorage cs = new ChemicalStorage();
         switch (bund) {
             case "Отдельный":
                 SeparateBund separateBund = new SeparateBund();
-                separateBund.setHeight(height);
+                try {
+                    separateBund.setHeight(Double.valueOf(height));
+                } catch (Exception ignored) {
+                    separateBund.setHeight((double) 1);
+                }
                 cs.setBund(separateBund);
                 break;
             case "Общий":
                 GroupBund groupBund = new GroupBund();
-                groupBund.setArea(area);
+                try {
+                    groupBund.setArea(Double.valueOf(area));
+                } catch (Exception ignored) {
+                    groupBund.setArea((double) 1);
+                }
                 cs.setBund(groupBund);
                 break;
             default:
@@ -47,25 +124,101 @@ public class KamiClient extends WebServiceGatewaySupport {
                 break;
         }
         cs.setChemicalAgentName(chemicalAgentName);
-        cs.setLatitude(latitude);
-        cs.setLongitude(longitude);
-        cs.setQuantity(quantity);
+        cs.setLatitude(Double.valueOf(latitude));
+        cs.setLongitude(Double.valueOf(longitude));
+        cs.setQuantity(Double.valueOf(quantity));
         cs.setQuantityUnit(quantityUnit);
-
-        VehicleCharacteristics vc = new VehicleCharacteristics();
-        vc.setFillingTime(fillingTime);
-        vc.setTravelTime(travelTime);
-        vc.setVolume(volume);
-        vc.setWorkTime(workTime);
-        vc.setUnit("мин");
 
         AhovRdTask request = new AhovRdTask();
         request.setIsPopulationInformed(isPopulationInformed);
-        request.setPredictionDateTime(xmlGregCal);
+
+        if (!predictionDateTime.isEmpty()) {
+            try {
+                request.setPredictionDateTime(StrToXMLGregorianCalendar(predictionDateTime));
+            } catch (Exception ignored) {
+                throw new Exception("Неверно заполнено поле Дата/время прогнозирования*");
+            }
+        } else
+            throw new Exception("Заполните поле Дата/время прогнозирования*");
+
         request.setChemicalStorage(cs);
-        request.setVehicleCharacteristics(vc);
-        request.setStartNeutralizationTime(DatatypeFactory.newInstance().newDuration(Duration.ofMinutes(startNeutralizationTime).toString()));
-        request.setTimeToNeutralization(DatatypeFactory.newInstance().newDuration(Duration.ofMinutes(timeToNeutralization).toString()));
+        if (isVehicleEnabled) {
+            VehicleCharacteristics vc = new VehicleCharacteristics();
+            try {
+                vc.setFillingTime(Double.valueOf(fillingTime));
+            } catch (Exception ignored) {
+
+            }
+            try {
+                vc.setTravelTime(Double.valueOf(travelTime));
+            } catch (Exception ignored) {
+
+            }
+            try {
+                vc.setVolume(Double.valueOf(volume));
+            } catch (Exception ignored) {
+
+            }
+            try {
+                vc.setWorkTime(Double.valueOf(workTime));
+            } catch (Exception ignored) {
+
+            }
+            vc.setUnit("мин");
+            request.setVehicleCharacteristics(vc);
+        }
+        try {
+            request.setStartNeutralizationTime(DatatypeFactory.newInstance().newDuration(Duration.ofMinutes(Long.valueOf(startNeutralizationTime)).toString()));
+        } catch (Exception ignored) {
+
+        }
+        try {
+            request.setTimeToNeutralization(DatatypeFactory.newInstance().newDuration(Duration.ofMinutes(Long.valueOf(timeToNeutralization)).toString()));
+        } catch (Exception ignored) {
+
+        }
+
+        request = (AhovRdTask) setCommonInfo(request, id, title, dateTime, advancedInfo, isWeatherEnabled, airPressure, airTemperature, atmosphereState, cloudiness, weatherDateTime, precipitation, windSpeed, windDirection, snowCover);
+        Execute e = new Execute();
+        e.setTask(request);
+
+        return (ExecuteResponse) getWebServiceTemplate()
+                .marshalSendAndReceive(e, new SoapActionCallback("http://kami.ru/ITaskOperationsContract/Execute"));
+    }
+
+    //-------------расчет задачи ВЗРЫВ----------------------------------------------------------------------------------
+    public ExecuteResponse getBlast(String beginningRescueOperation, String durationRescueOperation, String explosiveAgent, String latitude, String longitude, String massOfAgent, String id, String title, String dateTime, String advancedInfo, boolean isWeatherEnabled, String airPressure, String airTemperature, String atmosphereState, String cloudiness, String weatherDateTime, String precipitation, String windSpeed, String windDirection, boolean snowCover) throws Exception {
+        BlastTask request = new BlastTask();
+        try {
+            request.setBeginningRescueOperation(StrToXMLGregorianCalendar(beginningRescueOperation));
+        } catch (Exception ignored) {
+
+        }
+        try {
+            request.setDurationRescueOperation(Integer.valueOf(durationRescueOperation));
+        } catch (Exception ignored) {
+
+        }
+        if (!explosiveAgent.isEmpty())
+            request.setExplosiveAgent(explosiveAgent);
+        try {
+            request.setLatitude(Double.valueOf(latitude));
+        } catch (Exception ignored) {
+
+        }
+        try {
+            request.setLongitude(Double.valueOf(longitude));
+        } catch (Exception ignored) {
+
+        }
+        try {
+            request.setMassOfAgent(Double.valueOf(massOfAgent));
+        } catch (Exception ignored) {
+
+        }
+
+        request = (BlastTask)setCommonInfo(request, id, title, dateTime, advancedInfo, isWeatherEnabled, airPressure, airTemperature, atmosphereState, cloudiness, weatherDateTime, precipitation, windSpeed, windDirection, snowCover);
+
         Execute e = new Execute();
         e.setTask(request);
 
